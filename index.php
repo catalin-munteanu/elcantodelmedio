fb79b6efe72dfacf393d9e1137e40f0e87e10e75
Catalín Munteanu <catalinapampin@gmail.com>
2023-03-14 10:09:58 -0300

    Paso a tema de Wordpress

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="wp-content\themes\cantodelmedio\css\style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&display=swap" rel="stylesheet">
+    <script src="js/main.js"></script>
+    <title>El canto del medio</title>
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+
+    <main>
+
+        <section>
+
+            <h1 class="elementoSeccion1">El canto del medio</h1>
+
+            <img src="img\mobile\TIPITO.png" id="tipito" class="elementoSeccion1">
+
+            <h2 class="elementoSeccion1">Escuela de Canto Consciente y Visionario
+                <br>de <a id="paulaNaanim">Paula Naanim Telis</a>
+            </h2>
+
+        </section>
+
+        <section>
+
+            <h2>Formación integral</h2>
+
+            <!-- <details>
+                <summary>
+                    <h2>Formación integral</h2>
+                </summary>
+                <p>El programa de Formación está diseñado para que realices un recorrido de aprendizaje teórico-práctico
+                    de (re)conocimiento, estimulación y entrenamiento de las funciones neurofisiológicas que sustentan
+                    la emisión saludable del sonido vocal integrando la expresión emocional y el abordaje de las
+                    creencias mentales que sustentan nuestro canto. con el objetivo de que desarrolles un Canto
+                    Consciente y lo apliques en tu vida artística y creativa.</p><br>
+
+                <p>También contamos con una formación especial en Canto Visionario, con el objetivo de que aprendas a
+                    usar tu voz en estados alterados o expandidos de conciencia.</p><br>
+
+                <p>Conoce el programa completo</p>
+
+            </details> -->
+
+            <article>
+
+                <h3>Próximo curso en abril 2023</h3>
+
+                <p>Reservá tu lugar</p>
+
+            </article>
+
+        </section>
+
+        <section>
+
+            <h2>Consultoría para la voz</h2>
+
+        </section>
+
+        <section>
+
+            <h2>Hola, soy Paula Naanim Telis</h2>
+            <br>
+            <p>Cantante y compositora de música urbana y tribal, escritora, artista de performance y practicante de danzas
+            orientales y contemplativas.</p>
+            <br>
+            <p>Profesora de canto y Lic. En Musicoterapia (UBA) con perspectiva en género, comunidad, salud holística y
+            transpersonal.</p>
+            <br>
+            <p>Luego de más de 12 años de experiencia, más de 500 acompañamientos y del estudio comparado de usos y
+            funciones del cuerpo y la voz en distintas culturas y subculturas del mundo, creé El canto del medio para
+            difundir los hallazgos de mi investigación.</p>
+            <br>
+            <p>Considero que la práctica del canto consciente nos permite conocer y activar a nuestro canal central, desde
+            el cual es posible (re)unificarnos en la percepción de nuestro medio interno, externo y divino a partir del
+            uso de la voz. Ello nos pone en contacto con la emisión fisiológica, garantía de salud física y emocional al
+            servicio de la expresión, la comunicación y la creatividad. A partir de allí, también es posible desarrollar
+            un canto visionario.</p>
+            <br>
+            <p>Es mi deseo que descubras, percibas e integres los beneficios del canto para expandirlos, potenciarlos y
+            aplicarlos en tu vida artística y creativa.</p>
+            <br>
+            <img src="img/mobile/foto-bio-04.jpeg">
+
+        </section>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
