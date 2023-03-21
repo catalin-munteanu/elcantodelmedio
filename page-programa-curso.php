80cdc36ed3f39899018e39c5bbd399537558f8a4
Catalín Munteanu <catalinapampin@gmail.com>
2023-03-21 17:57:32 -0300

    Avances en pagina cursos

---
+++ page-programa-curso.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../wp-content/themes/cantodelmedio/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&family=Quicksand:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <script src="wp-content\themes\cantodelmedio\js\main.js"></script>
+    <title>El canto del medio | Cursos | Programa completo</title>
+</head>
+
+<body>
+
+    <header>
+        <ul id="menuRedesHeader">
+            <li class="iconRedesHeader"><a href="https://www.youtube.com/channel/UCLtgVMuTefGp63pZiPophPA" target="_blank"><img src="../wp-content/themes/cantodelmedio/img/mobile/logo-yt-nv.png" class="imgRedesHeader" id="logoYoutubeHeader"></a></li>
+            <li class="iconRedesHeader"><a href="https://www.instagram.com/elcantodelmedio/" target="_blank"><img src="../wp-content/themes/cantodelmedio/img/mobile/ig-prueba-2.png" class="imgRedesHeader" id="logoInstagramHeader"></a></li>
+            <li class="iconRedesHeader"><a href="https://wa.me/5491136148307" target="_blank"><img src="../wp-content/themes/cantodelmedio/img/mobile/wpp-prueba.png" class="imgRedesHeader" id="logoWhatsAppHeader"></a></li>
+        </ul>
+
+        <button id="burgerMenu"><img src="../wp-content/themes/cantodelmedio/img/mobile/burger-menu.png" id="burgerMenuImg"></button>
+
+        <nav id="mainMenu">
+            <ul>
+                <li><a href="#formacionIntegral">Formación</a></li>
+                <li><a href="#consultoriaVoz">Consultorías</a></li>
+                <li><a href="#bio">Bio</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+        <section id="sectProgramaCompleto">
+            <h1 id="progCompleto">Programa completo</h1>
+
+            <table id="tablaProgCompleto">
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>Capacitaciones en canto consciente (mensuales)</td>
+                    <td>Respiración expandida<br>Pasaje de la voz<br>Vibroacústica vocal</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Profundización en canto consciente (cuatrimestre)</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Iniciación al canto consciente (cuatrimestre)</td>
+                    <td></td>
+                    <td>Seminarios</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Profundización en canto visionario (cuatrimestre)</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Nivel 1</th>
+                    <th>Nivel 2</th>
+                    <th>Nivel 3</th>
+                </tr>
+            </table>
+
+        </section>
+
+        <section id="sectConscienteNivelUno">
+            <h2>Iniciación al canto consciente (*)</h2>
+            <h3>NIVEL 1</h3>
+
+            <article id="descripcionConscienteNivelUno">
+                <p>Este curso teórico - práctico está pensado para que aprendas las bases funcionales esenciales de la emisión neurofisiológica. Dichas funciones permiten la activación del canal principal, el canal del medio, desde el cual potenciamos el desarrollo de un canto consciente, saludable y musicalmente eficiente.</p>
+
+                <p>Modalidad: presencial y grupal<br>
+                    Duración: 4 meses (Abril - Julio 2023)<br>
+                    Cursada: Lunes de 19hs a 21hs<br>
+                    Lugar: Estudio Neo Baladi (zona Parque Centenario )<br>
+                    Valor: $6000 mensual</p>
+
+                <p>Orientado a cantantes-artistas y a toda persona interesada en integrar al canto como medio de auto-conocimento y creación</p>
+
+                <p>Inicia el lunes 3 de abril</p>
+
+                <p>Si queres conocer los contenidos del curso, hace click acá.</p>
+
+                <p>(*) El curso cuatrimestral de iniciación al Canto consciente corresponde al nivel 1 de formación obligatoria para acceder a los demás cursos facilitados por la escuela.</p>
+            </article>
+
+            <article id="contenidosConscienteNivelUno">
+                <h3>Contenidos del curso</h3>
+                <p>Los siguientes ítems serán desarrollados en relación con el estudio y entrenamiento del canal central de emisión fisiológica:</p>
+                <ul>
+                    <li>Estimulación propioceptiva de la relación entre postura, respiración y emisión</li>
+                    <li>Reconocimiento y diferenciación de hábitos compensatorios y funcionales en la relación entre postura, respiración y emisión</li>
+                    <li>Concientización de apoyos, ejes y alineación de los centros respiratorio, fonatorio y resonador</li>
+                    <li>Reconocimiento y elongación de la musculatura laríngea, respiratoria y expresiva</li>
+                    <li>Registro y entrenamiento de la ciclicidad respiración-emisión</li>
+                    <li>Reconocimiento y activación de las válvulas de cierre y apertura laríngeas y pélvicas</li>
+                    <li>Registro de las vías respiratoria superiores y entrenamiento de la base articulatoria</li>
+                    <li>Reconocimiento y diferenciación de los registros vocales grave, medio y agudo</li>
+                    <li>Exploración de cavidades vibro-acústicas de resonancia</li>
+                    <li>Reconocimiento y diferenciación de creencias condicionantes y estimulantes respecto a la voz y al canto</li>
+                </ul>
+                <p>Volver al programa completo</p>
+            </article>
+        </section>
+
+        <section id="sectConscienteNivelDos">
+
+            <article id="descripcionConscienteNivelDos">
+                <h2>Entrenamiento en canto consciente (cuatrimestre)</h2>
+                <h3>NIVEL 2</h3>
+
+                <p>Este curso teórico - práctico está diseñado para fortalecer las bases funcionales de emisión neurofisiológica desarrolladas en el nivel 1 y crear hábitos de práctica que permitan tanto la integración de lo estudiado como su aplicación en la vida artística y creativa.</p>
+
+                <p>Modalidad: presencial y grupal<br>
+                    Duración: 4 meses (Agosto - Noviembre 2023)<br>
+                    Cursada: a definir<br>
+                    Lugar: Estudio Neo Baladi (zona Parque Centenario)<br>
+                    Valor: a definir</p>
+
+                <p>Orientado a cantantes-artistas y a toda persona interesada en profundizar en el canto como medio de auto-conocimento y creación</p>
+            </article>
+            <article id="contenidosConscienteNivelDos">
+                <h3>Contenidos del curso</h3>
+                <p>Los siguientes ítems se desarrollarán en relación con la estimulación de hábitos córporo-vocales y mentales y de la creación de rutinas de práctica personalizadas con el objetivo de fortalecer las bases neurofisiológicas del canto consciente y conservar la activación del canal central de emisión</p>
+                <ul>
+                    <li>Desprogramación de hábitos compensatorios</li>
+                    <li>Fortalecimiento de hábitos funcionales</li>
+                    <li>Tonificación y flexibilización de la musculatura laríngea, respiratoria y expresiva</li>
+                    <li>Fortalecimiento de la ciclicidad respiración-emisión</li>
+                    <li>Fortalecimiento de la base articulatoria</li>
+                    <li>Entrenamiento de los registros grave, medio y agudo</li>
+                    <li>Utilización de cavidades vibro-acústicas de resonancia</li>
+                    <li>Flexibilización de la activación de las válvulas de cierre y apertura laríngeas y pélvicas</li>
+                </ul>
+            </article>
+        </section>
+        <section id="descripcionVisionarioNivelDos">
+        <article id="contenidosVisionarioNivelDos">
+            <h2>Entrenamiento en canto visionario (cuatrimestre)</h2>
+            <h3>Nivel 2</h3>
+
+            <p>El curso de Entrenamiento en Canto Visionario está diseñado para que aprendas a utilizar la voz en estados alterados o expandidos de consciencia, utilizando el canal del medio para canalizar música, palabra, sonido y simbología de forma activa y creativa, es decir, escuchando y (re)grabando información sutil.</p>
+
+            <ul>
+                <li>Modalidad: presencial y grupal</li>
+                <li>Duración: 4 meses (Agosto - Noviembre 2023)</li>
+                <li>Cursada: a definir</li>
+                <li>Lugar: Estudio Neo Baladi (zona Parque Centenario)</li>
+                <li>Valor: a definir</li>
+            </ul>
+            <p>Incluye material teórico y láminas complementarias</p>
+
+            <p>Orientado a cantantes-canalizadores y a toda persona interesada en integrar al canto como medio de lectura y escritura akáshica</p>
+        </article>
+        </section>
+    </main>
+</body>
+
+</html>
